a1bf9446ea868d25ac8fbf0021149f8280f2c27c
Zheng <ahzzheng@gmail.com>
2021-04-04 12:35:39 -0700

    Working remove and adding animations

--- Development/Testing projects/InterfaceSwiftUI/InterfaceSwiftUI/ContentView.swift
+++ Development/Testing projects/InterfaceSwiftUI/InterfaceSwiftUI/ContentView.swift
@@ -7,175 +7,185 @@
 
 import SwiftUI
 
-//class Card: ObservableObject, Identifiable {
-//    let id = UUID()
-//    
-//    @Published var added = false
-//    @Published var name: String
-//    @Published var color: Color
-//    @Published var sound: Sound
-//    
-//    init(name: String, color: Color, sound: Sound) {
-//        self.name = name
-//        self.color = color
-//        self.sound = sound
-//    }
-//}
-//
-//
-//struct CardsView: View {
-//    @State var cards = [
-//        Card(name: "Object", color: .green, sound: Sound(name: "Select a sound")),
-//    ]
-//    
-//    var body: some View {
-//        ScrollView(.horizontal, showsIndicators: false) {
-//            ScrollViewReader { proxy in
-//                HStack {
-//                    ForEach(cards.indices, id: \.self) { index in
-//                        CardView(card: cards[index], addPressed: {
-//                            withAnimation(.easeOut) {
-//                            cards.append(Card(name: "Object", color: .green, sound: Sound(name: "Select a sound")))
-//                            }
-//                            
-//                            
-//                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
-//                                withAnimation {
-//                                    proxy.scrollTo(cards.count - 1, anchor: .center)
-//                                }
-//                            }
-//                        }, removePressed: {
-//                            withAnimation(.easeOut) {
-//                                _ = cards.remove(at: index)
-//                            }
-//                        })
-//                        .frame(width: 300, height: 400)
-//                    }
-//                }
-//                .padding(.horizontal, 20)
-//            }
-//        }
-//    }
-//}
-//
-//struct CardView: View {
-//    
-//    var sounds = [
-//        Sound(name: "Chimes"),
-//        Sound(name: "Waves"),
-//        Sound(name: "Notes"),
-//        Sound(name: "Danger"),
-//        Sound(name: "Danger 2"),
-//        Sound(name: "J. S. Bach"),
-//        Sound(name: "Frédéric Chopin"),
-//        Sound(name: "R. Nathaniel Dett"),
-//        Sound(name: "Memento")
-//    
-//    ]
-//    
-//    @ObservedObject var card: Card
-//    
-//    var addPressed: (() -> Void)?
-//    var removePressed: (() -> Void)?
-//    
-//    var body: some View {
-//        VStack(spacing: 0) {
-//            Button(action: {
-//                print("press.")
-//                card.added.toggle()
-//                
-//                if card.added {
-//                    addPressed?()
-//                } else {
-//                    removePressed?()
-//                }
-//            }) {
-//                Text(card.added ? "Remove" : "Add")
-//                    .foregroundColor(Color.white)
-//                    .font(.system(size: 18, weight: .semibold))
-//                    .frame(maxWidth: .infinity)
-//                    .padding(.vertical, 10)
-//                    .background(card.added ? Color.red : Color.green)
-//                    .cornerRadius(12, corners: [.topLeft, .topRight])
-//                    .padding(.horizontal, 16)
-//            }
-//            
-//            VStack(alignment: .leading, spacing: 0) {
-//                TextField("Textfield", text: $card.name)
-//                    .foregroundColor(Color.white)
-//                    .font(.system(size: 32, weight: .semibold, design: .rounded))
-//                    
-//                    .padding(EdgeInsets(top: 6, leading: 20, bottom: 6, trailing: 20))
-//                
-//                
-//                HStack {
-//                    Text("Color")
-//                        .foregroundColor(.white)
-//                    
-//                    
-//                    Spacer()
-//                    
-//                    ColorPicker("Set the background color", selection: $card.color)
-//                        .labelsHidden()
-//                        .scaleEffect(x: 1.2, y: 1.2)
-//                        .offset(x: -2, y: 0)
-//                    
-//                }
-//                .padding(16)
-//                .background(Color(#colorLiteral(red: 0.3725216476, green: 0.6794671474, blue: 0.1888703918, alpha: 1)))
-//                .cornerRadius(12)
-//                .padding(EdgeInsets(top: 6, leading: 20, bottom: 6, trailing: 20))
-//                
-//                
-//                HStack {
-//                    Text("Sound")
-//                        .foregroundColor(.white)
-//                    
-//                    Spacer()
-//                    Picker(card.sound.name, selection: $card.sound) {
-//                        ForEach(sounds, id: \.self) {
-//                            Text($0.name)
-//                        }
-//                    }
-//                    .pickerStyle(MenuPickerStyle())
-//                    .foregroundColor(Color.white.opacity(0.8))
-//                    
-//                }
-//                .padding(16)
-//                .background(Color(#colorLiteral(red: 0.3725216476, green: 0.6794671474, blue: 0.1888703918, alpha: 1)))
-//                .cornerRadius(12)
-//                .padding(EdgeInsets(top: 6, leading: 20, bottom: 6, trailing: 20))
-//                
-//            }
-//            .frame(maxWidth: .infinity)
-//            .padding(.vertical, 16)
-//            .background(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
-//            .cornerRadius(16)
-//        }
-//        .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .bottom)))
-//    }
-//}
-//
-//struct CardsView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        CardsView()
-//            
-//    }
-//}
-//
-//
-//extension View {
-//    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
-//        clipShape( RoundedCorner(radius: radius, corners: corners) )
-//    }
-//}
-//struct RoundedCorner: Shape {
-//
-//    var radius: CGFloat = .infinity
-//    var corners: UIRectCorner = .allCorners
-//
-//    func path(in rect: CGRect) -> Path {
-//        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
-//        return Path(path.cgPath)
-//    }
-//}
+class Card: ObservableObject, Identifiable, Hashable {
+    let id = UUID()
+    
+    @Published var added = false
+    @Published var name: String
+    @Published var color: Color
+    @Published var sound: Sound
+    
+    init(name: String, color: Color, sound: Sound) {
+        self.name = name
+        self.color = color
+        self.sound = sound
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    static func ==(lhs: Card, rhs: Card) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
+
+struct CardsView: View {
+    
+    let cardWidth = CGFloat(300)
+    
+    @State var cards = [
+        Card(name: "Object", color: .green, sound: Sound(name: "Select a sound"))
+    ]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            ScrollViewReader { proxy in
+                HStack {
+                    ForEach(Array(cards.enumerated()), id: \.1) { (index, card) in
+                        CardView(card: card, addPressed: {
+                            
+                            withAnimation(.easeOut) {
+                                cards.append(Card(name: "Object", color: .green, sound: Sound(name: "Select a sound")))
+                            }
+                            
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                withAnimation {
+                                    proxy.scrollTo(cards.last?.id ?? card.id, anchor: .center)
+                                }
+                            }
+                        }, removePressed: {
+                            withAnimation(.easeOut) {
+                                _ = cards.remove(at: index)
+                            }
+                        })
+                        .id(card.id)
+                        .frame(width: cardWidth, height: 400)
+                    }
+                }
+                .padding(.horizontal, (UIScreen.main.bounds.width - cardWidth) / 2)
+            }
+        }
+    }
+}
+
+struct CardView: View {
+    
+    var sounds = [
+        Sound(name: "Chimes"),
+        Sound(name: "Waves"),
+        Sound(name: "Notes"),
+        Sound(name: "Danger"),
+        Sound(name: "Danger 2"),
+        Sound(name: "J. S. Bach"),
+        Sound(name: "Frédéric Chopin"),
+        Sound(name: "R. Nathaniel Dett"),
+        Sound(name: "Memento")
+    
+    ]
+    
+    @ObservedObject var card: Card
+    
+    var addPressed: (() -> Void)?
+    var removePressed: (() -> Void)?
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            Button(action: {
+                card.added.toggle()
+                
+                if card.added {
+                    addPressed?()
+                } else {
+                    removePressed?()
+                }
+            }) {
+                Text(card.added ? "Remove" : "Add")
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 18, weight: .semibold))
+                    .frame(maxWidth: .infinity)
+                    .padding(.vertical, 10)
+                    .background(card.added ? Color.red : Color.green)
+                    .cornerRadius(12, corners: [.topLeft, .topRight])
+                    .padding(.horizontal, 16)
+            }
+            
+            VStack(alignment: .leading, spacing: 0) {
+                TextField("Textfield", text: $card.name)
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 32, weight: .semibold, design: .rounded))
+                    
+                    .padding(EdgeInsets(top: 6, leading: 20, bottom: 6, trailing: 20))
+                
+                
+                HStack {
+                    Text("Color")
+                        .foregroundColor(.white)
+                    
+                    
+                    Spacer()
+                    
+                    ColorPicker("Set the background color", selection: $card.color)
+                        .labelsHidden()
+                        .scaleEffect(x: 1.2, y: 1.2)
+                        .offset(x: -2, y: 0)
+                    
+                }
+                .padding(16)
+                .background(Color(#colorLiteral(red: 0.3725216476, green: 0.6794671474, blue: 0.1888703918, alpha: 1)))
+                .cornerRadius(12)
+                .padding(EdgeInsets(top: 6, leading: 20, bottom: 6, trailing: 20))
+                
+                
+                HStack {
+                    Text("Sound")
+                        .foregroundColor(.white)
+                    
+                    Spacer()
+                    Picker(card.sound.name, selection: $card.sound) {
+                        ForEach(sounds, id: \.self) {
+                            Text($0.name)
+                        }
+                    }
+                    .pickerStyle(MenuPickerStyle())
+                    .foregroundColor(Color.white.opacity(0.8))
+                    
+                }
+                .padding(16)
+                .background(Color(#colorLiteral(red: 0.3725216476, green: 0.6794671474, blue: 0.1888703918, alpha: 1)))
+                .cornerRadius(12)
+                .padding(EdgeInsets(top: 6, leading: 20, bottom: 6, trailing: 20))
+                
+            }
+            .frame(maxWidth: .infinity)
+            .padding(.vertical, 16)
+            .background(Color(#colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)))
+            .cornerRadius(16)
+        }
+        .transition(.asymmetric(insertion: .move(edge: .trailing), removal: .move(edge: .bottom)))
+    }
+}
+
+struct CardsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CardsView()
+            
+    }
+}
+
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
+struct RoundedCorner: Shape {
+
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
--- Development/Testing projects/InterfaceSwiftUI/InterfaceSwiftUI/TestingView.swift
+++ Development/Testing projects/InterfaceSwiftUI/InterfaceSwiftUI/TestingView.swift
@@ -7,63 +7,164 @@
 
 import SwiftUI
 
-/// Ran into this problem: "SwiftUI ForEach index out of range error when removing row"
-/// `ObservableObject` solution from https://stackoverflow.com/a/62796050/14351818
-class Card: ObservableObject, Identifiable {
-    let id = UUID()
-    @Published var name: String
+//
+//struct CapsuleButtonFilled: View {
+//
+//    // MARK: - PROPERTIES
+//
+//    var backgroundColor: Color
+//
+//    // MARK:
+//
+//    var body: some View {
+//        Button(action: {}, label: {
+//            Text("The Solid")
+//                .foregroundColor(Color.black)
+//                .background(backgroundColor)
+//                .padding()
+//        })
+//
+//        .background(
+//            Capsule(style: .circular)
+//                .fill(backgroundColor)
+//        )
+//    }
+//}
+//
+//struct CapsuleButtonFilled_Previews: PreviewProvider {
+//    static var previews: some View {
+//        CapsuleButtonFilled(backgroundColor: .orange)
+//            .previewLayout(.sizeThatFits)
+//    }
+//}
 
-    init(name: String) {
-        self.name = name
-    }
-}
 
-struct ContentView: View {
-    @State var cards = [
-        Card(name: "Apple"),
-        Card(name: "Banana "),
-        Card(name: "Coupon"),
-        Card(name: "Dog"),
-        Card(name: "Eat")
-    ]
-    
-    var body: some View {
-        ScrollView(.horizontal) {
-            HStack {
-                
-                ForEach(cards.indices, id: \.self) { index in
-                    CardView(card: cards[index], removePressed: {
-                        
-                        withAnimation(.easeOut) {
-                            _ = cards.remove(at: index) /// remove the card
-                        }
-                        
-                    })
-                    .transition(.scale)
-                }
-                
-            }
-        }
-    }
-}
+//class Card: ObservableObject, Identifiable, Hashable {
+//    let id = UUID()
+//    @Published var name: String
+//
+//    init(name: String) {
+//        self.name = name
+//    }
+//    
+//    func hash(into hasher: inout Hasher) {
+//        hasher.combine(id)
+//    }
+//    static func ==(lhs: Card, rhs: Card) -> Bool {
+//        return lhs.id == rhs.id
+//    }
+//}
 
-struct CardView: View {
-    
-    @ObservedObject var card: Card
-    var removePressed: (() -> Void)?
-    
-    var body: some View {
-        Button(action: {
-            removePressed?() /// call the remove closure
-        }) {
-            VStack {
-                Text("Remove")
-                Text(card.name)
-            }
-        }
-        .foregroundColor(Color.white)
-        .font(.system(size: 24, weight: .medium))
-        .padding(40)
-        .background(Color.red)
-    }
-}
+//struct ContentView: View {
+//    @State var cards = [
+//        Card(name: "Apple"),
+//        Card(name: "Banana "),
+//        Card(name: "Coupon"),
+//        Card(name: "Dog"),
+//        Card(name: "Eat")
+//    ]
+//
+//    var body: some View {
+//        VStack {
+//            ScrollView(.horizontal) {
+//                ScrollViewReader { proxy in
+//                    HStack {
+//
+//                        ForEach(Array(cards.enumerated()), id: \.1) { (index, card) in
+//                            CardView(
+//                                card: cards[index],
+//                                scrollToEndPressed: {
+//                                    proxy.scrollTo(cards.count - 1, anchor: .center)
+//                                },
+//                                removePressed: {
+//
+//                                    withAnimation(.easeOut) {
+//                                        _ = cards.remove(at: index) /// remove the card
+//                                    }
+//
+//                                }
+//                            )
+//                            .transition(.scale)
+//                        }
+//
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+
+
+
+//
+//struct ContentView: View {
+//    @State var cards = [
+//        Card(name: "Apple"),
+//        Card(name: "Banana "),
+//        Card(name: "Coupon"),
+//        Card(name: "Dog"),
+//        Card(name: "Eat")
+//    ]
+//
+//    var body: some View {
+//        VStack {
+//            ScrollView(.horizontal) {
+//                ScrollViewReader { proxy in
+//                    HStack {
+//
+//                        ForEach(Array(cards.enumerated()), id: \.1) { (index, card) in
+//                            CardView(card: cards[index], scrollToEndPressed: {
+//                                
+//                                withAnimation(.easeOut) {
+//                                    proxy.scrollTo(cards.last?.id ?? card.id, anchor: .center) /// scroll to the last card's ID
+//                                }
+//                                
+//                            }, removePressed: {
+//                                
+//                                withAnimation(.easeOut) {
+//                                    _ = cards.remove(at: index) /// remove the card
+//                                }
+//
+//                            })
+//                            .id(card.id) /// add ID
+//                            .transition(.scale)
+//                        }
+//
+//                    }
+//                }
+//            }
+//        }
+//    }
+//}
+//
+//struct CardView: View {
+//    
+//    @ObservedObject var card: Card
+//    var scrollToEndPressed: (() -> Void)?
+//    var removePressed: (() -> Void)?
+//    
+//    var body: some View {
+//        VStack {
+//            Button(action: {
+//                scrollToEndPressed?() /// call the remove closure
+//            }) {
+//                VStack {
+//                    Text("Scroll to end")
+//                }
+//            }
+//            
+//            Button(action: {
+//                removePressed?() /// call the remove closure
+//            }) {
+//                VStack {
+//                    Text("Remove")
+//                    Text(card.name)
+//                }
+//            }
+//            .foregroundColor(Color.white)
+//            .font(.system(size: 24, weight: .medium))
+//            .padding(40)
+//            .background(Color.red)
+//        }
+//    }
+//}
