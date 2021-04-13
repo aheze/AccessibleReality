d5da6f9f301514a80f0e99c965e6c7d1d7efc56f
Zheng <ahzzheng@gmail.com>
2021-04-12 18:53:31 -0700

    Add base files for page One

---
+++ Development/Testing playgrounds/TemplateCopy/PlaygroundBook/Chapters/Chapter1.playgroundchapter/Pages/One.playgroundpage/main.swift
@@ -0,0 +1,58 @@
+//#-hidden-code
+//
+//  See LICENSE folder for this template’s licensing information.
+//
+//  Abstract:
+//  The Swift file containing the source code edited by the user of this playground book.
+//
+
+import UIKit
+import BookCore
+import PlaygroundSupport
+//#-end-hidden-code
+
+/*:
+ **Goal:** Use the Right-Hand Rule algorithm to navigate around walls.
+ 
+ # h1 test
+ ## h1 test
+ ### h1 test
+ #### h1 test
+ ##### h1 test
+ 
+Run this puzzle, and notice how your character stops after the first gem. The algorithm used here follows the [right-hand rule](glossary://right-hand%20rule) to move around walls. To solve the puzzle, you’ll need to tweak the algorithm, but first try using [pseudocode](glossary://pseudocode) to plan the action.
+
+Pseudocode looks a bit like [Swift](glossary://Swift) code, but it’s worded and structured so humans can easily understand it.
+ 
+    navigate around wall {
+       if blocked to the right {
+          move forward
+       } else {
+          turn right
+          move forward
+       }
+    }
+ 
+    while not on closed switch {
+       navigate around wall
+       if on gem {
+          collect gem
+          turn around
+       }
+    }
+    toggle switch
+ 
+ 1. steps: Based on the pseudocode above, write out a solution in code for the puzzle.
+ 2. Run your code and tweak your algorithm, if necessary, to solve the puzzle.
+ 
+ 1. Based on the pseudocode above, write out a solution in code for the puzzle.
+ 2. Run your code and tweak your algorithm, if necessary, to solve the puzzle.
+*/
+
+let str = "Hello, playground"
+
+//#-hidden-code
+//PlaygroundPage.current.liveView = instantiateLiveView()
+//PlaygroundPage.current.needsIndefiniteExecution = true
+//#-end-hidden-code
+
