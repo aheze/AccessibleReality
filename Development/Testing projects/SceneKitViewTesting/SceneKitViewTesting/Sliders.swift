f3a974c813f8f58ead1df5f6a94050fe1ac9f60d
Zheng <ahzzheng@gmail.com>
2021-04-13 11:48:15 -0700

    Work on geometry reader

--- Development/Testing projects/SceneKitViewTesting/SceneKitViewTesting/Sliders.swift
+++ Development/Testing projects/SceneKitViewTesting/SceneKitViewTesting/Sliders.swift
@@ -10,16 +10,21 @@ import SwiftUI
 struct OneSliderView: View {
     @ObservedObject var svm: SlidersViewModel
     var body: some View {
-        Sliders(
-            x: $svm.x,
-            y: $svm.y,
-            z: $svm.z,
-            name: "cubeNode",
-            min: "-100 cm",
-            max: "100 cm",
-            imageType: "Position"
-        )
-        .padding()
+        GeometryReader { proxy in
+            ScrollView {
+                Sliders(
+                    x: $svm.x,
+                    y: $svm.y,
+                    z: $svm.z,
+                    compactLayout: proxy.size.width < 450,
+                    name: "cubeNode",
+                    min: "-100 cm",
+                    max: "100 cm",
+                    imageType: "Position"
+                )
+                .padding()
+            }
+        }
     }
 }
 
@@ -28,29 +33,34 @@ struct TwoSliderView: View {
     @ObservedObject var svm2: SlidersViewModel
     
     var body: some View {
-        
-        HStack {
-            Sliders(
-                x: $svm1.x,
-                y: $svm1.y,
-                z: $svm1.z,
-                name: "cubeNode",
-                min: "-100 cm",
-                max: "100 cm",
-                imageType: "Position"
-            )
-            
-            Sliders(
-                x: $svm2.x,
-                y: $svm2.y,
-                z: $svm2.z,
-                name: "cameraNode",
-                min: "-100 cm",
-                max: "100 cm",
-                imageType: "Position"
-            )
+        GeometryReader { proxy in
+            ScrollView {
+                HStack {
+                    Sliders(
+                        x: $svm1.x,
+                        y: $svm1.y,
+                        z: $svm1.z,
+                        compactLayout: proxy.size.width < 900,
+                        name: "cubeNode",
+                        min: "-100 cm",
+                        max: "100 cm",
+                        imageType: "Position"
+                    )
+                    
+                    Sliders(
+                        x: $svm2.x,
+                        y: $svm2.y,
+                        z: $svm2.z,
+                        compactLayout: proxy.size.width < 900,
+                        name: "cameraNode",
+                        min: "-100 cm",
+                        max: "100 cm",
+                        imageType: "Position"
+                    )
+                }
+                .padding()
+            }
         }
-        .padding()
     }
 }
 
@@ -61,55 +71,63 @@ struct FourSliderView: View {
     @ObservedObject var svm4: SlidersViewModel
     
     var body: some View {
-        VStack {
-            HStack {
-                Sliders(
-                    x: $svm1.x,
-                    y: $svm1.y,
-                    z: $svm1.z,
-                    name: "cubeNode",
-                    min: "-100 cm",
-                    max: "100 cm",
-                    imageType: "Position"
-                )
-                
-                Sliders(
-                    x: $svm2.x,
-                    y: $svm2.y,
-                    z: $svm2.z,
-                    name: "cameraNode",
-                    min: "-100 cm",
-                    max: "100 cm",
-                    imageType: "Position"
-                )
-            }
-            HStack {
-                Sliders(
-                    x: $svm3.x,
-                    y: $svm3.y,
-                    z: $svm3.z,
-                    name: "cameraNode",
-                    min: "-360°",
-                    max: "360°",
-                    imageType: "Rotation"
-                )
-                
-                Sliders(
-                    x: $svm4.x,
-                    y: $svm4.y,
-                    z: $svm4.z,
-                    name: "directionNode",
-                    min: "-100 cm",
-                    max: "100 cm",
-                    imageType: "Position"
-                )
-                .brightness(-0.25)
-                .allowsHitTesting(false)
-                
-                
+        GeometryReader { proxy in
+            ScrollView {
+                VStack {
+                    HStack {
+                        Sliders(
+                            x: $svm1.x,
+                            y: $svm1.y,
+                            z: $svm1.z,
+                            compactLayout: proxy.size.width < 900,
+                            name: "cubeNode",
+                            min: "-100 cm",
+                            max: "100 cm",
+                            imageType: "Position"
+                        )
+                        
+                        Sliders(
+                            x: $svm2.x,
+                            y: $svm2.y,
+                            z: $svm2.z,
+                            compactLayout: proxy.size.width < 900,
+                            name: "cameraNode",
+                            min: "-100 cm",
+                            max: "100 cm",
+                            imageType: "Position"
+                        )
+                    }
+                    
+                    HStack {
+                        Sliders(
+                            x: $svm3.x,
+                            y: $svm3.y,
+                            z: $svm3.z,
+                            compactLayout: proxy.size.width < 900,
+                            name: "cameraNode",
+                            min: "-360°",
+                            max: "360°",
+                            imageType: "Rotation"
+                        )
+                        
+                        Sliders(
+                            x: $svm4.x,
+                            y: $svm4.y,
+                            z: $svm4.z,
+                            compactLayout: proxy.size.width < 900,
+                            name: "directionNode",
+                            min: "-100 cm",
+                            max: "100 cm",
+                            imageType: "Position"
+                        )
+                        .brightness(-0.25)
+                        .allowsHitTesting(false)
+                        
+                    }
+                }
+                .padding()
             }
         }
-        .padding()
     }
 }
 
@@ -119,6 +137,7 @@ struct Sliders: View {
     @Binding var y: Double
     @Binding var z: Double
     
+    var compactLayout = false
     let name: String
     let min: String
     let max: String
@@ -126,32 +145,16 @@ struct Sliders: View {
     
     
     var body: some View {
-        GeometryReader { proxy in
-            VStack {
-                
-                let _ = SlidersViewModel.didChange?()
-                
-                if proxy.size.width < 450 {
-                    VStack {
-                        
-                        HStack {
-                            Image(imageType)
-                            
-                            VStack(alignment: .leading, spacing: 2) {
-                                Text(name)
-                                    .font(.system(size: 19, weight: .medium, design: .monospaced))
-                                Text(imageType)
-                            }
-                        }
-                        
-                        Text("(\(Int(x)) x, \(Int(y)) y, \(Int(z)) z)")
-                            .font(.system(size: 28, weight: .medium))
-                        
-                    }
-                } else {
+        
+        
+        VStack {
+            
+            let _ = SlidersViewModel.didChange?()
+            
+            if compactLayout {
+                VStack {
+                    
                     HStack {
-                        
-                        
                         Image(imageType)
                         
                         VStack(alignment: .leading, spacing: 2) {
@@ -159,81 +162,98 @@ struct Sliders: View {
                                 .font(.system(size: 19, weight: .medium, design: .monospaced))
                             Text(imageType)
                         }
-                        
-                        Spacer()
-                        
-                        Text("(\(Int(x)) x, \(Int(y)) y, \(Int(z)) z)")
-                            .font(.system(size: 28, weight: .medium))
-                        
                     }
+                    
+                    Text("(\(Int(x)) x, \(Int(y)) y, \(Int(z)) z)")
+                        .font(.system(size: 28, weight: .medium))
+                    
                 }
-                
-                
+            } else {
                 HStack {
-                    Text("X")
-                        .foregroundColor(.white)
-                        .font(.system(size: 21, weight: .medium))
-                        .padding()
-                        .background(Color.red)
                     
                     
-                    Slider(value: $x, in: -100...100)
-                        .accentColor(Color.red)
-                        .padding(.trailing, 12)
-                }
-                .background(
-                    Color(.systemBackground)
-                )
-                .cornerRadius(16)
-                
-                HStack {
-                    Text("Y")
-                        .foregroundColor(.white)
-                        .font(.system(size: 21, weight: .medium)).padding()
-                        .background(Color.green)
+                    Image(imageType)
                     
-                    Slider(value: $y, in: -100...100)
-                        .accentColor(Color.green)
-                        .padding(.trailing, 12)
-                }
-                .background(
-                    Color(.systemBackground)
-                )
-                .cornerRadius(16)
-                
-                HStack {
-                    Text("Z")
-                        .foregroundColor(.white)
-                        .font(.system(size: 21, weight: .medium)).padding()
-                        .background(Color.blue)
-                    
-                    
-                    
-                    Slider(value: $z, in: -100...100)
-                        .accentColor(Color.blue)
-                        .padding(.trailing, 12)
-                }
-                .background(
-                    Color(.systemBackground)
-                )
-                .cornerRadius(16)
-                
-                
-                HStack {
-                    Text(min)
-                        .offset(x: 50, y: 0)
+                    VStack(alignment: .leading, spacing: 2) {
+                        Text(name)
+                            .font(.system(size: 19, weight: .medium, design: .monospaced))
+                        Text(imageType)
+                    }
                     
                     Spacer()
                     
-                    Text(max)
-                        .offset(x: -15, y: 0)
+                    Text("(\(Int(x)) x, \(Int(y)) y, \(Int(z)) z)")
+                        .font(.system(size: 28, weight: .medium))
+                    
                 }
-                
             }
-            .padding(20)
-            .background(Color(.secondarySystemBackground))
+            
+            
+            HStack {
+                Text("X")
+                    .foregroundColor(.white)
+                    .font(.system(size: 21, weight: .medium))
+                    .padding()
+                    .background(Color.red)
+                
+                
+                Slider(value: $x, in: -100...100)
+                    .accentColor(Color.red)
+                    .padding(.trailing, 12)
+            }
+            .background(
+                Color(.systemBackground)
+            )
             .cornerRadius(16)
+            
+            HStack {
+                Text("Y")
+                    .foregroundColor(.white)
+                    .font(.system(size: 21, weight: .medium)).padding()
+                    .background(Color.green)
+                
+                Slider(value: $y, in: -100...100)
+                    .accentColor(Color.green)
+                    .padding(.trailing, 12)
+            }
+            .background(
+                Color(.systemBackground)
+            )
+            .cornerRadius(16)
+            
+            HStack {
+                Text("Z")
+                    .foregroundColor(.white)
+                    .font(.system(size: 21, weight: .medium)).padding()
+                    .background(Color.blue)
+                
+                
+                
+                Slider(value: $z, in: -100...100)
+                    .accentColor(Color.blue)
+                    .padding(.trailing, 12)
+            }
+            .background(
+                Color(.systemBackground)
+            )
+            .cornerRadius(16)
+            
+            
+            HStack {
+                Text(min)
+                    .offset(x: 50, y: 0)
+                
+                Spacer()
+                
+                Text(max)
+                    .offset(x: -15, y: 0)
+            }
+            
         }
+        .padding(20)
+        .background(Color(.secondarySystemBackground))
+        .cornerRadius(16)
+        
     }
 }
 
